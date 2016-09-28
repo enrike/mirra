7d5ce26f5d50174f54309c24c6d7bbc5887d6d37
enrike <enrikehurtadomendieta@gmail.com>
2016-09-28 15:07:39 +0200

    aading the qt file example again

---
+++ examples/qtgui.py
@@ -0,0 +1,26 @@
+from PyQt4 import QtCore, QtGui
+
+
+app = None
+qtwin = None
+
+
+def do(win):
+    global qtwin
+    qtwin = win
+
+    # FILE
+    fm = win.menuBar().addMenu("&File")
+    fm.addAction(
+        QtGui.QAction("O&pen", win, shortcut="Ctrl+O", triggered=openFile)
+    )
+
+
+
+def openFile():
+    filename = QtGui.QFileDialog.getOpenFileName(qtwin, 'OpenFile', "./", "text files (*.txt)")
+    print filename
+
+
+
+
